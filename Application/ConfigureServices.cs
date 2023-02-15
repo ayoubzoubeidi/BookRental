a890181d1a3dadee93bdf9fc9d860dd0228123bd
Ayoub <ayoubzoubeidi@gmail.com>
2023-02-15 10:58:16 +0100

    Added Dto validation
    Implemented Dto validation as a IPipelineBehavior

--- Application/ConfigureServices.cs
+++ Application/ConfigureServices.cs
@@ -1,14 +1,18 @@
 ﻿using System.Reflection;
+using Application.Common.Behaviours;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application
 {
-    public static class DependencyInjection
+    public static class ConfigureServices
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             return services;
         }
     }
